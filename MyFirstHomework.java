b4530d8eed6084d3bd82ce51bbe78c660270c0ae
Kushausobaku <128152114+Kushausobaku@users.noreply.github.com>
2023-03-18 13:46:48 +0200

    Update MyFirstHomework.java

--- MyFirstHomework.java
+++ MyFirstHomework.java
@@ -8,17 +8,11 @@ public class MyFirstHomework {
 		Scanner u = new Scanner(System.in);
 		System.out.println("Input five digits in this field");
 		int input = u.nextInt();
-		int a;
-		int b;
-		int c;
-		int d;
-		int f;
-
-		a = input / 10000;
-		b = input % 10000 / 1000;
-		c = input % 1000 / 100;
-		d = input % 100 / 10;
-		f = input % 10 / 1;
+		int a = input / 10000;
+		int b = input % 10000 / 1000;
+		int c = input % 1000 / 100;
+		int d = input % 100 / 10;
+		int f = input % 10 / 1;
 
 		System.out.println(a + "\n" + b + "\n" + c + "\n" + d + "\n" + f);
 	}
