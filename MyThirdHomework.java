1d080989cf4e0c008882aa3e7c0d15260c185a3e
Kushausobaku <128152114+Kushausobaku@users.noreply.github.com>
2023-03-18 13:36:52 +0200

    Add files via upload

---
+++ MyThirdHomework.java
@@ -0,0 +1,17 @@
+package first;
+
+import java.util.Scanner;
+
+public class MyThirdHomework {
+
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		System.out.println("Input the radius of a circle");
+		
+		int r = scann.nextInt();
+		double length  = 2 * Math.PI * r;
+		
+		System.out.println(length);
+	}
+
+}
